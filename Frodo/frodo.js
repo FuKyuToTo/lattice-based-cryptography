1ad2bdd10bbc3234a9b2e5b21691dbc209b25894
Krit <40591036+FuKyuToTo@users.noreply.github.com>
2018-07-09 14:53:41 +0900

    Update frodo.js

--- Frodo/frodo.js
+++ Frodo/frodo.js
@@ -1,4 +1,4 @@
-//<script type="text/javascript" src="Utils/prng.js"></script>
+//<script type="text/javascript" src="../Utils/prng.js"></script>
 //<script type="text/javascript" src="frodo_random_values.js"></script>
 //=======================================================================================
 function initMatrixDefault(x, y) {
