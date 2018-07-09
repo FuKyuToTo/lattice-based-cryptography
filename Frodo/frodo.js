407fc9926c6cb7e06d19ee5d0f49191b66f01a56
Krit <40591036+FuKyuToTo@users.noreply.github.com>
2018-07-09 14:51:16 +0900

    Update frodo.js

--- Frodo/frodo.js
+++ Frodo/frodo.js
@@ -1,389 +1,219 @@
 //<script type="text/javascript" src="Utils/prng.js"></script>
 //<script type="text/javascript" src="frodo_random_values.js"></script>
 //=======================================================================================
-var TestMatrix = function() {
-    var x = 0;
-    var y = 0;
-    var matrix = [];
-
-    var initMatrix_default = function(m, n) {
-        x = m;
-        y = n;
-        for (var i = 0; i < m; i++) {
-        	matrix[i] = [];
-        }
-    };
-    
-    var initMatrix_random = function(m, n, q) {
-		this.initMatrix_default(m, n);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < n; j++) {
-				matrix[i][j] = nextInt(q);
-			}
-		}
-	};
-	
-    var initMatrix_range = function(m, n, low, high) {
-		this.initMatrix_default(m, n);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < n; j++) {
-				matrix[i][j] = rangeValue(low, high);
-			}
-		}
-	};
-	
-	var initMatrix_identity = function(m, n) {
-		this.initMatrix_default(m, n);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < n; j++) {
-				matrix[i][j] = (i == j ? 1 : 0);
-			}
-		}
-	};
-    
-    var initMatrix_2d_check = function(arr) {
-		x = arr.length;
-		y = arr[0].length;
-		for (var i = 0; i < m; i++) {
-			if (arr[i].length != n) {
-				alert("All rows must have the same length");
-		        return;
-			}
-		}
-		matrix = arr;
-	};
-	
-    var initMatrix_2d_uncheck = function(arr, m, n) {
-		matrix = arr;
-		x = m;
-		y = n;
-	};
-	
-	var initMatrix_expansion = function(arr, m) {
-		x = m;
-		y = (m != 0 ? arr.length / m : 0);
-		if (m * y != arr.length) {
-			alert("Array length must be a multiple of m");
-			return;
-		}
-		this.initMatrix_default(m, y);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < y; j++) {
-				if(undefined == arr[i + j * m]) {
-					matrix[i][j] = 0.0;
-				} else {
-					matrix[i][j] = arr[i + j * m];
-				}
-			}
-		}
-	};
-//	-------------------------------------------public method-------------------------------------------
-	var getMatrix = function() {
-		return matrix;
-	};
-	
-	var set = function(m, n, current) {
-		matrix[m][n] = current;
-	};
-	
-	var get = function(m, n) {
-		return matrix[m][n];
-	};
-	
-	//A'
-	var transpose = function() {
-		var X = TestMatrix();
-		X.initMatrix_default(y, x);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[j][i] = matrix[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//C = A + B
-	var add = function(B) {
-		checkMatrixDimensions(B);
-		var X = TestMatrix();
-		X.initMatrix_default(x, y);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[i][j] = matrix[i][j] + B.matrix()[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//C = A - B
-	var substract = function(B) {
-		checkMatrixDimensions(B);
-		var X = TestMatrix();
-		X.initMatrix_default(x, y);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[i][j] = matrix[i][j] - B.matrix()[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//Multiply a matrix by a scalar, C = s*A
-	var scalarMultiply = function(s) {
-		var X = TestMatrix();
-		X.initMatrix_default(x, y);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[i][j] = s * matrix[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//C = A * B
-	var multiply = function(B) {
-		if (B.x() != y) {
-			alert("Matrix inner dimensions must agree");
-			return;
-		}
-		var X = TestMatrix();
-		X.initMatrix_default(x, B.y());
-		var C = X.getMatrix();
-		var Bcolj = [];
-		for (var j = 0; j < B.y(); j++) {
-			for (var k = 0; k < y; k++) {
-				Bcolj[k] = B.matrix()[k][j];
-			}
-			for (var i = 0; i < x; i++) {
-				var Arowi = matrix[i];
-				var s = 0;
-				for (var k = 0; k < y; k++) {
-					s += Arowi[k] * Bcolj[k];
-				}
-				C[i][j] = s;
-			}
-		}
-		return X;
-	};
-
-	//Multiply a matrix B by a vevtor a, c = a * B
-	var vectorMultiplyMatrix = function(B) {
-		if (B.x() != y) {
-			alert("Matrix inner dimensions must agree");
-			return;
-		}
-		
-		var Av = getMatrix()[0];
-		
-		var v = new Array(B.y());
-		for (var j = 0; j < B.y(); j++) {
-			v[j] = 0;
-		}
-		
-		for (var i = 0; i < y; i++) {
-			var Browi = B.matrix()[i];
-			for (var j = 0; j < B.y(); j++) {
-				v[j] += Browi[j]*Av[i];
-			}
-		}
-		
-		var X = TestMatrix();
-		X.initMatrix_default(1, B.y());
-		var C = X.getMatrix();
-		C[0] = v;
-		return X;
-	};
-	
-	
-	//C = A * B mod q
-	var multiplyMod = function(B, q) {
-		if (B.x() != y) {
-			alert("Matrix inner dimensions must agree");
-			return;
-		}
-		var X = TestMatrix();
-		X.initMatrix_default(x, B.y());
-		var C = X.getMatrix();
-		var Bcolj = [];
-		for (var j = 0; j < B.y(); j++) {
-			for (var k = 0; k < y; k++) {
-				Bcolj[k] = B.matrix()[k][j];
-			}
-			for (var i = 0; i < x; i++) {
-				var Arowi = matrix[i];
-				var s = 0;
-				for (var k = 0; k < y; k++) {
-					s += Arowi[k] * Bcolj[k];
-				}
-				C[i][j] = s % q;
-				if (C[i][j] < 0) {
-					C[i][j] += q;
-				}
-				if (C[i][j] == 0) {
-					C[i][j] = 0;
-				}
-			}
-		}
-		return X;
-	};
-	
-	var mod = function(q) {
-		if (q <= 0) {
-			alert("modulus is nonpositive");
-			return
-		}
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				matrix[i][j] %= q;
-				if (matrix[i][j] < 0) {
-					matrix[i][j] += q;
-				}
-			}
-		}
-	};
-	
-//	-------------------------------------------private method-------------------------------------------
-	function nextInt(q) {
-	    return Math.floor(random() * q);	//prng.js -> random()
+function initMatrixDefault(x, y) {
+	var matrix = new Array(x);
+	for (var i = 0; i < x; i++) {
+       	matrix[i] = new Array(y);
 	}
-	
-	function rangeValue(low, high) {
-		return Math.floor(random() * (high - low + 1) + low);
-	}
-	
-    function shuffle(arr) {
-    	var arr2 = arr.slice();
-    	for(var j, x, i = arr2.length; i; j = parseInt(random() * i), x = arr2[--i], arr2[i] = arr2[j], arr2[j] = x);
-    	return arr2;    
-    }
-    
-	//Check if size(A) == size(B)
-	function checkMatrixDimensions(B) {
-		if (B.x() != x || B.y() != y) {
-			alert("Matrix dimensions must agree");
-			return;
-		}
-	}
-
-    return {
-      matrix:function() {return matrix;},
-      x:function() {return x;},
-      y:function() {return y;},
-      //
-      initMatrix_default:initMatrix_default,
-      initMatrix_random:initMatrix_random,
-      initMatrix_range:initMatrix_range,
-	  initMatrix_identity:initMatrix_identity,
-      initMatrix_2d_check:initMatrix_2d_check,
-      initMatrix_2d_uncheck:initMatrix_2d_uncheck,
-      initMatrix_expansion:initMatrix_expansion,
-      
-      getMatrix:getMatrix,
-      set:set,
-      get:get,
-      transpose:transpose,
-      add:add,
-      substract:substract,
-      scalarMultiply:scalarMultiply,
-      multiply:multiply,
-      vectorMultiplyMatrix:vectorMultiplyMatrix,
-      multiplyMod:multiplyMod,
-      mod:mod
-    };
-};
-//=======================================================================================
-var am;
-var bm;
-var sm;
-var bb;
-var cc;
-
-var arr_v;
-var k1matrix;
-var k2matrix;
-
-function alice0() {
-    var amatrix = TestMatrix();
-	var smatrix = TestMatrix();
-	var ematrix = TestMatrix();
-
-    amatrix.initMatrix_random(n, n, q);
-	smatrix.initMatrix_2d_uncheck(ss, n, l);
-	ematrix.initMatrix_2d_uncheck(ee, n, l);
-	
-	var bmatrix = TestMatrix();
-	bmatrix = amatrix.multiply(smatrix).add(ematrix);
-	bmatrix.mod(q);
-	
-	am = amatrix;
-	bm = bmatrix;
-	sm = smatrix;
+	return matrix;
 }
 
-function bob() {
-	var amatrix = am;
-	var bmatrix = bm;
-	
-	var s1matrix = TestMatrix();	// Z^m*n
-	var e1matrix = TestMatrix();	// Z^m*n
-	var e2matrix = TestMatrix();	// Z^m*l
-	
-	s1matrix.initMatrix_2d_uncheck(ss1, m, n);
-	e1matrix.initMatrix_2d_uncheck(ee1, m, n);
-	e2matrix.initMatrix_2d_uncheck(ee2, m, l);
-	
-	var b1matrix = s1matrix.multiply(amatrix);
-	var vmatrix = s1matrix.multiply(bmatrix);
-	
-    b1matrix = b1matrix.add(e1matrix);  // Z^m*n
-    b1matrix.mod(q);
-    vmatrix = vmatrix.add(e2matrix);    // Z^m*l
-	vmatrix.mod(q);
-	
-	var newC = [];
-	var newV = [];
-	for (var i = 0; i < vmatrix.getMatrix().length; i++) {
-		newC[i] = vmatrix.getMatrix()[i].slice();
-		newV[i] = vmatrix.getMatrix()[i].slice();
+function initMatrixRandom(x, y, q) {
+	var matrix = new Array(x);
+	for (var i = 0; i < x; i++) {
+       	matrix[i] = new Array(y);
+		for (var j = 0; j < y; j++) {
+			matrix[i][j] = nextInt(q);
+		}
 	}
+	return matrix;
+}
+//***************************************************
+//A'
+function transpose(A) {
+	var A_x = A.length;
+	var A_y = A[0].length;
 	
-	var cmatrix = TestMatrix();    // Z^m*l
-	k1matrix = TestMatrix();     // Z^m*l
-	cmatrix.initMatrix_2d_uncheck(newC, m, l);
-	k1matrix.initMatrix_2d_uncheck(newV, m, l);
+	var C = initMatrixDefault(A_y, A_x);
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			C[j][i] = A[i][j];
+		}
+	}
+	return C;
+}
 	
-	var arr_c = cmatrix.getMatrix();
-	var arr_k1 = k1matrix.getMatrix();
-    for (var i = 0; i < m; i++) {
-        for (var j = 0; j < l; j++) {
-            arr_c[i][j] = (arr_c[i][j] >> 10) & 1;	// >> logq - b - 1
-            //arr_c[i][j] = Math.floor(arr_c[i][j] * 0.0009765625) % 2;
-			
-			arr_k1[i][j] = (arr_k1[i][j] + 1024) % q;
-            arr_k1[i][j] >>= 11;  // >>= logq - b
-            //arr_k1[i][j] = Math.round(arr_k1[i][j] * 0.00048828125) % 16;
-        }
-    }
+//C = A + B
+function addMod(A, B, q) {
+	checkDimensions(A, B);
+	var A_x = A.length;
+	var A_y = A[0].length;
 	
-	bb = b1matrix;
-	cc = cmatrix;
+	var C = initMatrixDefault(A_x, A_y);
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			C[i][j] = (A[i][j] + B[i][j]) % q;
+			if (C[i][j] < 0) {
+				C[i][j] += q;
+			}
+		}
+	}
+	return C;
+}
+	
+//C = A - B
+function substractMod(A, B, q) {
+	checkDimensions(A, B);
+	var A_x = A.length;
+	var A_y = A[0].length;
+	
+	var C = initMatrixDefault(A_x, A_y);
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			C[i][j] = (A[i][j] - B[i][j]) % q;
+			if (C[i][j] < 0) {
+				C[i][j] += q;
+			}
+		}
+	}
+	return C;
+}
+	
+//Multiplies a matrix by a scalar, C = s*A
+function scalarMultiplyMod(s, A, q) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	
+	var C = initMatrixDefault(A_x, A_y);
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			C[i][j] = (s * A[i][j]) % q;
+			if (C[i][j] < 0) {
+				C[i][j] += q;
+			}
+		}
+	}
+	return C;
 }
 
-function alice1() {
-	var b1m = bb;
-	var cm = cc;
-	var smatrix = sm;
+//Matrix multiplication, C = A * B
+function multiply(A, B) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	var B_x = B.length;
+	var B_y = B[0].length;
 	
-	var b1s = b1m.multiply(smatrix);    // Z^m*l
-	b1s.mod(q);
-    k2matrix = b1s;     // Z^m*l
-	rec(k2matrix.getMatrix(), m, l, 11, cm.getMatrix());
+	if (B_x != A_y) {
+		alert("Matrix inner dimensions must agree");
+		return;
+	}
+	
+	var C = initMatrixDefault(A_x, B_y);
+	var Bcolj = new Array(A_y);
+	for (var j = 0; j < B_y; j++) {
+		for (var k = 0; k < A_y; k++) {
+			Bcolj[k] = B[k][j];
+		}
+		for (var i = 0; i < A_x; i++) {
+			var Arowi = A[i];
+			var s = 0;
+			for (var k = 0; k < A_y; k++) {
+				s += Arowi[k] * Bcolj[k];
+			}
+			C[i][j] = s;
+		}
+	}
+	return C;
 }
-//--------------------------------------------------------------------------------------
+
+//C = A * B mod q
+function multiplyMod(A, B, q) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	var B_x = B.length;
+	var B_y = B[0].length;
+	
+	if (B_x != A_y) {
+		alert("Matrix inner dimensions must agree");
+		return;
+	}
+	
+	var C = initMatrixDefault(A_x, B_y);
+	var Bcolj = new Array(A_y);
+	for (var j = 0; j < B_y; j++) {
+		for (var k = 0; k < A_y; k++) {
+			Bcolj[k] = B[k][j];
+		}
+		for (var i = 0; i < A_x; i++) {
+			var Arowi = A[i];
+			var s = 0;
+			for (var k = 0; k < A_y; k++) {
+				s += Arowi[k] * Bcolj[k];
+			}
+			C[i][j] = s % q;
+			if (C[i][j] < 0) {
+				C[i][j] += q;
+			}
+		}
+	}
+	return C;
+}
+
+//Multiplies a matrix B by a vector a, c = a * B
+function vectorMultiplyMatrix(a, B) {
+	//var A_x = 1;
+	var A_y = a.length;
+	var B_x = B.length;
+	var B_y = B[0].length;
+	
+	if (B_x != A_y) {
+		alert("Matrix inner dimensions must agree");
+		return;
+	}
+		
+	var v = new Array(B_y);
+	for (var j = 0; j < B_y; j++) {
+		v[j] = 0;
+	}
+		
+	for (var i = 0; i < A_y; i++) {
+		var Browi = B[i];
+		for (var j = 0; j < B_y; j++) {
+			v[j] += a[i] * Browi[j];
+		}
+	}
+	return v;
+}
+
+function mod(A, q) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	if (q <= 0) {
+		alert("modulus not positive");
+		return;
+	}
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			A[i][j] %= q;
+			if (A[i][j] < 0) {
+				A[i][j] += q;
+			}
+		}
+	}
+}
+
+//Checks if size(A) == size(B)
+function checkDimensions(A, B) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	var B_x = B.length;
+	var B_y = B[0].length;
+	
+	if (B_x != A_x || B_y != A_y) {
+		alert("Matrix dimensions must agree");
+		return;
+	}
+}
+
+function nextInt(q) {
+	return Math.floor(Math.random() * q);
+}
+
+function rangeValue(low, high) {
+	return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
 function rec(b1s, m, l, a, h) {
 	var whole = 1 << a;
 	var mask = whole - 1;
@@ -395,7 +225,7 @@ function rec(b1s, m, l, a, h) {
         for (var j = 0; j < l; j++) {
 			var remainder = b1s[i][j] & mask;
 			var use_hint = ((remainder + quarter) >> (a - 1)) & 1;
-			//h: the hint of cmatrix
+			//h: the hint cmatrix
 			var shift = use_hint * (2 * h[i][j] - 1) * quarter;
 			// if use_hint = 1 and h = 0, adding -quarter forces rounding down
 			//                     h = 1, adding quarter forces rounding up
@@ -406,30 +236,102 @@ function rec(b1s, m, l, a, h) {
 		}
 	}
 }
-//------------------------------------------- start testfrodo -------------------------------------------
-var b = 4;
-	l = 8,
-	m = 8,
-	n = 752,
-	q = 32768;	//logq = 15
+//=======================================================================================
+var am;
+var bm;
+var sm;
+var bb;
+var cc;
+var arr_v;
+var k1matrix = new Array(m);    // V, Z^m*l
+var k2matrix;
 
+function alice0(l, n, q) {
+	//A, n*n
+	var amatrix = initMatrixRandom(n, n, q);
+	var smatrix = ss;	//n*l
+	var ematrix = ee;	//n*l
+	//B = AS + E mod q, n*l
+	var bmatrix = multiply(amatrix, smatrix);
+	bmatrix = addMod(bmatrix, ematrix, q);
+	
+	am = amatrix;
+	bm = bmatrix;
+	sm = smatrix;
+}
+
+function bob(l, m, n, q) {
+	var amatrix = am;
+	var bmatrix = bm;
+	
+	var s1matrix = ss1;	// Z^m*n
+	var e1matrix = ee1;	// Z^m*n
+	var e2matrix = ee2;	// Z^m*l
+	
+	var b1matrix = multiply(s1matrix, amatrix);
+	var vmatrix = multiply(s1matrix, bmatrix);
+	
+	b1matrix = addMod(b1matrix, e1matrix, q);  // Z^m*n
+	vmatrix = addMod(vmatrix, e2matrix, q);    // Z^m*l
+	var cmatrix = new Array(m);    // Z^m*l
+	
+	for (var i = 0; i < l; i++) {
+		cmatrix[i] = vmatrix[i].slice();
+		k1matrix[i] = vmatrix[i].slice();
+	}
+	
+	for (var i = 0; i < m; i++) {
+		for (var j = 0; j < l; j++) {
+			cmatrix[i][j] = (cmatrix[i][j] >> 10) & 1;	// >> logq - b - 1
+			//cmatrix[i][j] = Math.floor(cmatrix[i][j] * 0.0009765625) % 2;
+			
+			k1matrix[i][j] = (k1matrix[i][j] + 1024) % q;
+			k1matrix[i][j] >>= 11;  // >>= logq - b
+			//k1matrix[i][j] = Math.round(k1matrix[i][j] * 0.00048828125) % 16;
+		}
+	}
+	
+	bb = b1matrix;
+	cc = cmatrix;
+}
+
+function alice1(l, m, q) {
+	var b1m = bb;
+	var cm = cc;
+	var smatrix = sm;
+	
+	var b1s = multiplyMod(b1m, smatrix, q);    // Z^m*l
+	k2matrix = b1s;     // Z^m*l
+	rec(k2matrix, m, l, 11, cm);
+}
+//------------------------------------------- start frodo -------------------------------------------
+var m = 8,
+	n = 752,
+	l = 8,
+	a = 11,
+	b = 4;
+	q = 32768,
+	logq = 15;
+	sigma = 1.3229;
+	
 function testfrodo() {
 	print("Test Frodo:");
 	print("Input:");
-	print("b = " + b);
-	print("l = " + l);
 	print("m = " + m);
 	print("n = " + n);
+	print("l = " + l);
+	print("b = " + b);
 	print("q = " + q);
+	print("sigma = " + sigma);
 	print("Output:");
 	
-	alice0();
-	bob();
-	alice1();
-	
-	var ka = k2matrix.getMatrix().toString();
+	alice0(l, n, q);
+	bob(l, m, n, q);
+	alice1(l, m, q);
+
+	var ka = k2matrix.toString();
 	print("k_a = " + ka);
-	var kb = k1matrix.getMatrix().toString();
+	var kb = k1matrix.toString();
 	print("k_b = " + kb);
 	
 	if(ka == kb) {
@@ -437,7 +339,6 @@ function testfrodo() {
 	} else {
 		print("Failed");
 	}
-	print("");
 }
 //***********************************************************
 function print(message) {
