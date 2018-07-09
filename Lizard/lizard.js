7e7ec5a87d23cf850485e5a8d00261f1b62c6552
Krit <40591036+FuKyuToTo@users.noreply.github.com>
2018-07-09 14:27:35 +0900

    Update lizard.js

--- Lizard/lizard.js
+++ Lizard/lizard.js
@@ -1,294 +1,306 @@
 //<script type="text/javascript" src="Utils/prng.js"></script>
 //<script type="text/javascript" src="lizard_random_values.js"></script>
 //===============================================================================
-var TestMatrix = function() {
-    var x = 0;
-    var y = 0;
-    var matrix = [];
+function initMatrixDefault(x, y) {
+	var matrix = new Array(x);
+	for (var i = 0; i < x; i++) {
+       	matrix[i] = new Array(y);
+	}
+	return matrix;
+}
 
-    var initMatrix_default = function(m, n) {
-        x = m;
-        y = n;
-        for (var i = 0; i < m; i++) {
-        	matrix[i] = [];
-        }
-    };
+function initMatrixRandom(x, y, q) {
+	var matrix = new Array(x);
+	for (var i = 0; i < x; i++) {
+       	matrix[i] = new Array(y);
+		for (var j = 0; j < y; j++) {
+			matrix[i][j] = nextInt(q);
+		}
+	}
+	return matrix;
+}
 
-    var initMatrix_random = function(m, n, q) {
-		this.initMatrix_default(m, n);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < n; j++) {
-				matrix[i][j] = nextInt(q);
+//A'
+function transpose(A) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	
+	var C = initMatrixDefault(A_y, A_x);
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			C[j][i] = A[i][j];
+		}
+	}
+	return C;
+}
+	
+//C = A + B
+function addMod(A, B, q) {
+	checkDimensions(A, B);
+	var A_x = A.length;
+	var A_y = A[0].length;
+	
+	var C = initMatrixDefault(A_x, A_y);
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			C[i][j] = (A[i][j] + B[i][j]) % q;
+			if (C[i][j] < 0) {
+				C[i][j] += q;
 			}
 		}
-	};
+	}
+	return C;
+}
 	
-    var initMatrix_range = function(m, n, low, high) {
-		this.initMatrix_default(m, n);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < n; j++) {
-				matrix[i][j] = rangeValue(low, high);
+//c = a - b
+function vectorSubstract(a, b) {
+	if (b.length != a.length) {
+		alert("Vector length must agree");
+		return;
+	}
+	
+	var c = new Array(a.length);
+	for (var i = 0; i < a.length; i++) {
+		c[i] = a[i] - b[i];
+	}
+	return c;
+}
+	
+//Multiplies a vector by a scalar, c = s*v
+function scalarMultiplyVector(s, v) {
+	var c = new Array(v.length);
+	for (var i = 0; i < v.length; i++) {
+		c[i] = s * v[i];
+	}
+	return c;
+}
+
+//Multiplies a matrix by a scalar, C = s*A
+function scalarMultiplyMod(s, A, q) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	
+	var C = initMatrixDefault(A_x, A_y);
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			C[i][j] = (s * A[i][j]) % q;
+			if (C[i][j] < 0) {
+				C[i][j] += q;
 			}
+			//if (C[i][j] == 0) {
+			//	C[i][j] = 0;
+			//}
 		}
-	};
+	}
+	return C;
+}
+
+//C = A * B
+function multiply(A, B) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	var B_x = B.length;
+	var B_y = B[0].length;
 	
-	var initMatrix_identity = function(m, n) {
-		this.initMatrix_default(m, n);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < n; j++) {
-				matrix[i][j] = (i == j ? 1 : 0);
-			}
-		}
-	};
-    
-    var initMatrix_2d_check = function(arr) {
-		x = arr.length;
-		y = arr[0].length;
-		for (var i = 0; i < m; i++) {
-			if (arr[i].length != n) {
-				alert("All rows must have the same length");
-		        return;
-			}
-		}
-		matrix = arr;
-	};
+	if (B_x != A_y) {
+		alert("Matrix inner dimensions must agree");
+		return;
+	}
 	
-    var initMatrix_2d_uncheck = function(arr, m, n) {
-		matrix = arr;
-		x = m;
-		y = n;
-	};
-	
-	var initMatrix_expansion = function(arr, m) {
-		x = m;
-		y = (m != 0 ? arr.length / m : 0);
-		if (m * y != arr.length) {
-			alert("Array length must be a multiple of m");
-			return;
+	var C = initMatrixDefault(A_x, B_y);
+	var Bcolj = new Array(A_y);
+	for (var j = 0; j < B_y; j++) {
+		for (var k = 0; k < A_y; k++) {
+			Bcolj[k] = B[k][j];
 		}
-		this.initMatrix_default(m, y);
-		for (var i = 0; i < m; i++) {
-			for (var j = 0; j < y; j++) {
-				if(undefined == arr[i + j * m]) {
-					matrix[i][j] = 0.0;
+		for (var i = 0; i < A_x; i++) {
+			var Arowi = A[i];
+			var s = 0;
+			for (var k = 0; k < A_y; k++) {
+				if (Bcolj[k] == 0) {
+					//s += 0;
+				} else if (Bcolj[k] == 1) {
+					s += Arowi[k];
+				} else if (Bcolj[k] == -1) {
+					s -= Arowi[k];
 				} else {
-					matrix[i][j] = arr[i + j * m];
-				}
-			}
-		}
-	};
-//	-------------------------------------------public method-------------------------------------------
-	var getMatrix = function() {
-		return matrix;
-	};
-	
-	var set = function(m, n, val) {
-		matrix[m][n] = val;
-	};
-	
-	var get = function(m, n) {
-		return matrix[m][n];
-	};
-	
-	//A'
-	var transpose = function() {
-		var X = TestMatrix();
-		X.initMatrix_default(y, x);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[j][i] = matrix[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//C = A + B
-	var add = function(B) {
-		checkMatrixDimensions(B);
-		var X = TestMatrix();
-		X.initMatrix_default(x, y);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[i][j] = matrix[i][j] + B.matrix()[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//C = A - B
-	var substract = function(B) {
-		checkMatrixDimensions(B);
-		var X = TestMatrix();
-		X.initMatrix_default(x, y);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[i][j] = matrix[i][j] - B.matrix()[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//Multiply a matrix by a scalar, C = s*A
-	var scalarMultiply = function(s) {
-		var X = TestMatrix();
-		X.initMatrix_default(x, y);
-		var C = X.getMatrix();
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				C[i][j] = s * matrix[i][j];
-			}
-		}
-		return X;
-	};
-	
-	//C = A * B
-	var multiply = function(B) {
-		if (B.x() != y) {
-			alert("Matrix inner dimensions must agree");
-			return;
-		}
-		var X = TestMatrix();
-		X.initMatrix_default(x, B.y());
-		var C = X.getMatrix();
-		var Bcolj = [];
-		for (var j = 0; j < B.y(); j++) {
-			for (var k = 0; k < y; k++) {
-				Bcolj[k] = B.matrix()[k][j];
-			}
-			for (var i = 0; i < x; i++) {
-				var Arowi = matrix[i];
-				var s = 0;
-				for (var k = 0; k < y; k++) {
 					s += Arowi[k] * Bcolj[k];
 				}
-				C[i][j] = s;
 			}
+			C[i][j] = s;
 		}
-		return X;
-	};
+	}
+	return C;
+}
 
-	//Multiply a matrix B by a vevtor a, c = a * B
-	var vectorMultiplyMatrix = function(B) {
-		if (B.x() != y) {
-			alert("Matrix inner dimensions must agree");
-			return;
-		}
-		var Av = getMatrix()[0];
-		var v = new Array(B.y());
-		for (var j = 0; j < B.y(); j++) {
-			v[j] = 0;
-		}
-		for (var i = 0; i < y; i++) {
-			var Browi = B.matrix()[i];
-			for (var j = 0; j < B.y(); j++) {
-				v[j] += Browi[j]*Av[i];
-			}
-		}
-		var X = TestMatrix();
-		X.initMatrix_default(1, B.y());
-		var C = X.getMatrix();
-		C[0] = v;
-		return X;
-	};
+//C = A * B mod q
+function multiplyMod(A, B, q) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	var B_x = B.length;
+	var B_y = B[0].length;
 	
-	//C = A * B mod q
-	var multiplyMod = function(B, q) {
-		if (B.x() != y) {
-			alert("Matrix inner dimensions must agree.");
-			return;
+	if (B_x != A_y) {
+		alert("Matrix inner dimensions must agree");
+		return;
+	}
+	
+	var C = initMatrixDefault(A_x, B_y);
+	var Bcolj = new Array(A_y);
+	for (var j = 0; j < B_y; j++) {
+		for (var k = 0; k < A_y; k++) {
+			Bcolj[k] = B[k][j];
 		}
-		var X = TestMatrix();
-		X.initMatrix_default(x, B.y());
-		var C = X.getMatrix();
-		var Bcolj = [];
-		for (var j = 0; j < B.y(); j++) {
-			for (var k = 0; k < y; k++) {
-				Bcolj[k] = B.matrix()[k][j];
-			}
-			for (var i = 0; i < x; i++) {
-				var Arowi = matrix[i];
-				var s = 0;
-				for (var k = 0; k < y; k++) {
+		for (var i = 0; i < A_x; i++) {
+			var Arowi = A[i];
+			var s = 0;
+			for (var k = 0; k < A_y; k++) {
+				if (Bcolj[k] == 0) {
+					//s += 0;
+				} else if (Bcolj[k] == 1) {
+					s += Arowi[k];
+				} else if (Bcolj[k] == -1) {
+					s -= Arowi[k];
+				} else {
 					s += Arowi[k] * Bcolj[k];
 				}
-				C[i][j] = s % q;
-				if (C[i][j] < 0) {
-					C[i][j] += q;
-				}
-				if (C[i][j] == 0) {
-					C[i][j] = 0;
-				}
+			}
+			C[i][j] = s % q;
+			if (C[i][j] < 0) {
+				C[i][j] += q;
 			}
 		}
-		return X;
-	};
-	
-	var mod = function(q) {
-		if (q <= 0) {
-			alert("Modulus is nonpositive");
-			return
-		}
-		for (var i = 0; i < x; i++) {
-			for (var j = 0; j < y; j++) {
-				matrix[i][j] %= q;
-				if (matrix[i][j] < 0) {
-					matrix[i][j] += q;
-				}
-			}
-		}
-	};
-//	-------------------------------------------private method-------------------------------------------
-	function nextInt(q) {
-	    return Math.floor(random() * q);	//prng.js -> random()
 	}
-	
-	function rangeValue(low, high) {
-		return Math.floor(random() * (high - low + 1) + low);
-	}
-	
-    	function shuffle(arr) {
-    		var arr2 = arr.slice();
-    		for(var j, x, i = arr2.length; i; j = parseInt(random() * i), x = arr2[--i], arr2[i] = arr2[j], arr2[j] = x);
-    		return arr2;    
-  	}
-    
-	function checkMatrixDimensions(B) {
-		if (B.x() != x || B.y() != y) {
-			alert("Matrix dimensions must agree");
-			return;
-		}
-	}
+	return C;
+}
 
-    return {
-      matrix:function() {return matrix;},
-      x:function() {return x;},
-      y:function() {return y;},
-      //
-      initMatrix_default:initMatrix_default,
-      initMatrix_random:initMatrix_random,
-      initMatrix_range:initMatrix_range,
-	  initMatrix_identity:initMatrix_identity,
-      initMatrix_2d_check:initMatrix_2d_check,
-      initMatrix_2d_uncheck:initMatrix_2d_uncheck,
-      initMatrix_expansion:initMatrix_expansion,
-      
-      getMatrix:getMatrix,
-      set:set,
-      get:get,
-      transpose:transpose,
-      add:add,
-      substract:substract,
-      scalarMultiply:scalarMultiply,
-      multiply:multiply,
-      vectorMultiplyMatrix:vectorMultiplyMatrix,
-      multiplyMod:multiplyMod,
-      mod:mod
-    };
-};
-//---------------------------------------------------------------------------------------------------
+//Encrypt
+//Multiplies a matrix B by a vector a, c = a * B
+function encVectorMultiplyMatrix(a, B) {
+	//var A_x = 1;
+	var A_y = a.length;
+	var B_x = B.length;
+	var B_y = B[0].length;
+	
+	if (B_x != A_y) {
+		alert("Matrix inner dimensions must agree");
+		return;
+	}
+		
+	var v = new Array(B_y);
+	for (var j = 0; j < B_y; j++) {
+		v[j] = 0;
+	}
+		
+	for (var i = 0; i < A_y; i++) {
+		var Browi = B[i];
+		for (var j = 0; j < B_y; j++) {
+			if (a[i] == 0) {
+				//v[j] += 0;
+			} else if (a[i] == 1) {
+				v[j] += Browi[j];	
+			} else if (a[i] == -1) {
+				v[j] -= Browi[j];
+			} else {
+				v[j] += a[i] * Browi[j];
+			}
+		}
+	}
+	return v;
+}
+
+
+//Decrypt
+//Multiplies a matrix B by a vector a, c = a * B
+function decVectorMultiplyMatrix(a, B) {
+	//var A_x = 1;
+	var A_y = a.length;
+	var B_x = B.length;
+	var B_y = B[0].length;
+	
+	if (B_x != A_y) {
+		alert("Matrix inner dimensions must agree");
+		return;
+	}
+		
+	var v = new Array(B_y);
+	for (var j = 0; j < B_y; j++) {
+		v[j] = 0;
+	}
+		
+	for (var i = 0; i < A_y; i++) {
+		var Browi = B[i];
+		for (var j = 0; j < B_y; j++) {
+			/*
+			if (Browi[j] == 0) {
+				//v[j] += 0;
+			} else if (Browi[j] == 1) {
+				v[j] += a[i];
+			} else if (Browi[j] == -1) {
+				v[j] -= a[i];
+			} else {
+			*/
+				v[j] += a[i] * Browi[j];	// it will be a little bit faster for decrypt on Firefox
+			//}
+		}
+	}
+	return v;
+}
+
+
+function mod(A, q) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	if (q <= 0) {
+		alert("modulus not positive");
+		return;
+	}
+	for (var i = 0; i < A_x; i++) {
+		for (var j = 0; j < A_y; j++) {
+			A[i][j] %= q;
+			if (A[i][j] < 0) {
+				A[i][j] += q;
+			}
+		}
+	}
+}
+
+//Checks if size(A) == size(B)
+function checkDimensions(A, B) {
+	var A_x = A.length;
+	var A_y = A[0].length;
+	var B_x = B.length;
+	var B_y = B[0].length;
+	
+	if (B_x != A_x || B_y != A_y) {
+		alert("Matrix dimensions must agree");
+		return;
+	}
+}
+
+function nextInt(q) {
+	return Math.floor(Math.random() * q);
+}
+
+function rangeValue(low, high) {
+	return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+function shuffle(arr) {
+    var arr2 = arr.slice();
+    for(var j, x, i = arr2.length; i; j = parseInt(Math.random() * i), x = arr2[--i], arr2[i] = arr2[j], arr2[j] = x);
+    return arr2;    
+}
+
+function randomPlaintext () {
+	var plaintext = new Array(l);
+	for (var i = 0; i < l; i++) {
+       	plaintext[i] = nextInt(2);
+	}
+	vvector = plaintext;
+	m_transpose = scalarMultiplyVector(128, vvector);
+}
+//***********************************************************
 var am;
 var bm;
 var sm;
@@ -296,76 +308,72 @@ var vvector;
 var av;
 var bv;
 var resultvector;
+var m_transpose;
 
-function keyGeneration() {
-	var amatrix = TestMatrix();
-	var smatrix = TestMatrix();
-	var ematrix = TestMatrix();
+function keyGeneration(l, m, n, q) {
+	//A, m*n
+    var amatrix = initMatrixRandom(m, n, q);
+    //S, n*l
+	var smatrix = ss;
+	//E, m*l
+	var ematrix = ee;
 	//B = AS + E mod q, m*l
-	var bmatrix = TestMatrix();
-	
-	//amatrix.initMatrix_random(m, n, q);
-	amatrix.initMatrix_2d_uncheck(aa, m, n);
-	smatrix.initMatrix_2d_uncheck(ss, n, l);
-	ematrix.initMatrix_2d_uncheck(ee, m, l);
-	
-	bmatrix = amatrix.multiply(smatrix).add(ematrix);
-	bmatrix.mod(q);
+	var bmatrix = multiply(amatrix, smatrix);
+	bmatrix = addMod(bmatrix, ematrix, q);
 	
 	am = amatrix;
 	bm = bmatrix;
 	sm = smatrix;
 }
 
-function encrypt() {
+function encrypt(l, n, p, q) {
 	var amatrix = am;
 	var bmatrix = bm;
-	var rvector_transpos = TestMatrix();	// Z^1*m
-	rvector_transpos.initMatrix_expansion(rr, 1);
 	
-	var c1_prime_transpos = rvector_transpos.vectorMultiplyMatrix(amatrix);
-	var c2_prime_transpos = rvector_transpos.vectorMultiplyMatrix(bmatrix);
-	var c1_transpos = c1_prime_transpos.scalarMultiply(0.25);
+	var rvector_transpose = rr;	// Z^1*m
+	var c1_prime_transpose = encVectorMultiplyMatrix(rvector_transpose, amatrix);
+	var c2_prime_transpose = encVectorMultiplyMatrix(rvector_transpose, bmatrix);
 	
-	var arr_c1 = c1_transpos.getMatrix();
-	for (var i = 0; i < arr_c1[0].length; i++) {
-		arr_c1[0][i] = Math.round(arr_c1[0][i]);
+	var c1_transpose = new Array(n);
+	for (var i = 0; i < n; i++) {
+		c1_prime_transpose[i] = c1_prime_transpose[i] % q;
+		c1_transpose[i] = Math.round(0.25 * c1_prime_transpose[i]) % p;
+		if (c1_transpose[i] < 0) {
+			c1_transpose[i] += p;
+		}
 	}
 	
-	var m_transpos = vvector.scalarMultiply(128);
-	var c2_transpos = (c2_prime_transpos.scalarMultiply(0.25)).add(m_transpos);
-	
-	var arr_c2 = c2_transpos.getMatrix();
-	for (var i = 0; i < arr_c2[0].length; i++) {
-		arr_c2[0][i] = Math.round(arr_c2[0][i]);
+	var c2_transpose = new Array(l);
+	for (var i = 0; i < l; i++) {
+		c2_prime_transpose[i] = c2_prime_transpose[i] % q;
+		c2_transpose[i] = Math.round(m_transpose[i] + (0.25 * c2_prime_transpose[i])) % p;
+		if (c2_transpose[i] < 0) {
+			c2_transpose[i] += p;
+		}
 	}
 	
-	av = c1_transpos;
-	bv = c2_transpos;
+	av = c1_transpose;
+	bv = c2_transpose;
 }
 
-function decrypt() {
-	var c1vector_transpos = av;
-	var c2vector_transpos = bv;
+function decrypt(l, q, t) {
+	var c1vector_transpose = av;
+	var c2vector_transpose = bv;
 	var smatrix = sm;
-	
-	var c1TS = c1vector_transpos.vectorMultiplyMatrix(smatrix);
-	var resultvector_transpos = c2vector_transpos.substract(c1TS);
-	resultvector = resultvector_transpos.scalarMultiply(0.0078125);	// t/p = 1/128
-	
-	var arr = resultvector.getMatrix();
-	for (var i = 0; i < arr[0].length; i++) {
-		arr[0][i] = Math.round(arr[0][i]);
+
+	var c1TS = decVectorMultiplyMatrix(c1vector_transpose, smatrix);
+	var resultvector_transpose = vectorSubstract(c2vector_transpose, c1TS);
+	//resultvector = scalarMultiply(0.0078125, resultvector_transpose);	// t/p = 1/128
+	resultvector = new Array(l);
+	for (var i = 0; i < l; i++) {
+		resultvector[i] = Math.round(0.0078125 * resultvector_transpose[i]) % t;
+		if (resultvector[i] < 0) {
+			resultvector[i] += t;
+		}
 	}
-	resultvector.mod(t);
 }
-//--------------------------------------------------------------------------------------
-function randomPlaintext () {
-	var plaintext = TestMatrix();
-	plaintext.initMatrix_random(1, l, 2);
-	vvector = plaintext;
-}
-//--------------------------------------------------------------------------------------
+//===============================================================================
+//------------------------------------------- start lizard -------------------------------------------
 var m = 960,
 	n = 608,
 	t = 2,
@@ -375,6 +383,7 @@ var m = 960,
 	q = 1024,
 	h = 128,
 	alpha = 0.000363;
+
 function testlizard() {
 	print("Test Lizard:");
 	print("Input:");
@@ -384,25 +393,25 @@ function testlizard() {
 	print("t = " + t);
 	print("p = " + p);
 	print("q = " + q);
+	print("h = " + h);
+	print("α = " + alpha);
 	
 	randomPlaintext();
-	var ms = vvector.getMatrix().toString();
-	print("plain text = " + ms);
+	keyGeneration(l, m, n, q);
+	encrypt(l, n, p, q);
+	decrypt(l, q, t);
+	
 	print("Output:");
+	var ms = vvector.toString();
+	print("plain text =  " + ms);
+	var ts = resultvector.toString();
+	print("resulttext = " + ts);
 	
-	keyGeneration();
-	encrypt();
-	decrypt();
-	
-	var rs = resultvector.getMatrix().toString();
-	print("result = " + rs);
-	
-	if(rs == ms) {
+	if(ts == ms) {
 		print("Success!");
 	} else {
 		print("Failed");
 	}
-	print("");
 }
 //***********************************************************
 function print(message) {
